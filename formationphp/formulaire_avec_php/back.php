84a90ff36bd5194321cf64eaa86dfb70e68e2df2
Mourtada10 <mourtada@outlook.com>
2026-03-13 09:50:35 +0100

    edit

--- formationphp/formulaire_avec_php/back.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*if(isset($_POST["name"])){
-    echo "Le formulaire a été envoyé !" . "<br>";
-}
-
-$nouveauMembre = $_POST["name"];
-
-echo "Nouveau membre enrégistré : $nouveauMembre" . "<br>";
